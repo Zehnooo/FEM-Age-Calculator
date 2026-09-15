67fbe668a7522fa51e94e8f428bc1707d16f9063
Zehno <126801501+Zehnooo@users.noreply.github.com>
2026-09-15 12:55:42 -0500

    Added calculator object with method to calculate time difference between given date and current date

---
+++ src/scripts/calculator.js
@@ -0,0 +1,45 @@
+export default {
+    day: null,
+    month: null,
+    year: null,
+
+    setValue(key, value){ this[key] = Number(value); },
+    returnCurrentDate(){
+        const d = new Date();
+        d.setHours(0, 0, 0, 0)
+        return d;
+        },
+    returnCalcDate(){
+        return new Date(this.year, this.month - 1, this.day)
+    },
+
+    formatTime(current, given){
+        let years = given.getFullYear() - current.getFullYear();
+        let months = given.getMonth() - current.getMonth();
+        let days = given.getDate() - current.getDate();
+
+        if (days < 0) {
+            months--;
+
+            const previousMonth = new Date(
+                given.getFullYear(),
+                given.getMonth(),
+                0
+            );
+
+            days += previousMonth.getDate();
+        }
+
+        if (months < 0) {
+            years--;
+            months += 12;
+        }
+
+
+        return {
+            years,
+            months,
+            days
+        };
+    }
+}
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,8 @@
 console.log('init');
+import calc from './calculator.js';
+
+calc.setValue("day", "15");
+calc.setValue("month", "9");
+calc.setValue("year", "2026");
+
+console.log(calc.formatTime(calc.returnCalcDate(), calc.returnCurrentDate()));
