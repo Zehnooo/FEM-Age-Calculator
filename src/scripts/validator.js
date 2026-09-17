e7d769d6c5a3d4eaaf4b1f67cb627133a59ce2c4
Zehno <126801501+Zehnooo@users.noreply.github.com>
2026-09-17 16:28:05 -0500

    added length check to validator and updated return messages

--- src/scripts/validator.js
+++ src/scripts/validator.js
@@ -4,46 +4,50 @@ export default {
     day: {
         check(v){
             v = Number(v);
-            if (v < 1 || v > 31){ return { success: false, message: 'Invalid day of month.' }; }
+            if (v < 1 || v > 31){ return { success: false, message: 'Invalid day of month' }; }
             return { success: true };
             },
     },
     month: {
         check(v){
             v = Number(v);
-            if (v < 1 || v > 12){ return { success: false, message: 'Invalid month.' };  }
+            if (v < 1 || v > 12){ return { success: false, message: 'Invalid month' };  }
             return { success: true };
         }
     },
     year: {
         check(v) {
             v = Number(v);
-            if (v > calc.returnCurrentDate().getFullYear()){ return { success: false, message: 'Year cannot be in the future.' };
+            if (v > calc.returnCurrentDate().getFullYear()){ return { success: false, message: 'Year cannot be in the future' };
             }
             return { success: true };
         },
+
     },
     fullDate: {
         checkFuture(year, month, date){
             const future = new Date(year, month - 1, date) > calc.returnCurrentDate();
-            if (future === true) { return { success: false, message: 'Date cannot be in the future.' }; }
+            if (future === true) { return { success: false, message: 'Date cannot be in the future' }; }
             return { success: true };
         },
         checkMonthDays(year, month, date){
             const max = new Date(year, month, 0);
             const invalid = date > max.getDate();
-            if (invalid === true) { return { success: false, message: `There are only ${max.getDate()} days in ${max.toLocaleString('en-US', { month: 'long' })}.` }; }
+            if (invalid === true) { return { success: false, message: `There are only ${max.getDate()} days in ${max.toLocaleString('en-US', { month: 'long' })}` }; }
             return { success: true };
         }
     },
     middleware: {
         checkNull(v){
-            if (!v || v === '') {
-            return { success: false, message: 'Empty or invalid value.' };
-        } return { success: true };
+            if (!v || v === '') { return { success: false, message: 'Input cannot be blank' }; }
+            return { success: true };
         },
         checkNum(v){
-            if (isNaN(v)) { return { success: false, message: 'Value must be a numerical value.' }; }
+            if (isNaN(v)) { return { success: false, message: 'Value must be a numerical value' }; }
+            return { success: true };
+        },
+        checkLength(v, length) {
+            if (v.length !== length) { return { success: false, message: `Value must be ${length} digits` }; }
             return { success: true };
         }
     }
